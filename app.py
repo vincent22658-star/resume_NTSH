0caad62fa9cc96994114626f503fecfa279a9e19
chenjuiyi1110-stack <chenjuiyi1110@gmail.com>
2025-10-28 11:36:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
 # 建立問答集Store questions and answers in a simple list for demonstration purposes
 questions_answers = {
     "蘋果": "apple",
